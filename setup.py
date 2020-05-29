496602f5625f464f6f21a57ddde0a550273369f8
Gabriel Borges <gabrielsmborges@gmail.com>
2020-05-29 15:34:01 +0200

    créer le dossier gallerie

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import os
+os.system('mkdir gallerie')
 os.system('pip install opencv-python')
 os.system('pip install kivy')
 os.system('pip install --upgrade pip wheel setuptools')
 os.system('pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew')
 os.system('pip install kivy.deps.gstreamer')
 os.system('pip install kivy.deps.angle')
-os.system('pip install --upgrade kivy')
